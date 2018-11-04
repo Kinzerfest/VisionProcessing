eaf3ab33dd1dd8873eb3d8eb109dcdebc766a469
Kevin Tran <26168327+Kinzerfest@users.noreply.github.com>
2018-11-04 16:37:27 -0600

    Update src/Processing.java

--- src/Processing.java
+++ src/Processing.java
@@ -1,6 +1,5 @@
 import java.io.IOException;
-
-import org.opencv.core.Core;
+import org.opencv.core.*;
 import org.opencv.core.Mat;
 import org.opencv.core.Rect;
 import org.opencv.imgproc.Imgproc;
@@ -25,17 +24,20 @@ public class Processing {
         videoCapture.open(1);
         Mat input = new Mat();
         videoCapture.read(input);
-        //httpStreamService = new HttpStreamServer(input);
-        //httpStreamService.startStreamingServer();
+        httpStreamService = new HttpStreamServer(input);
+        httpStreamService.startStreamingServer();
         while (videoCapture.isOpened()) {
             videoCapture.read(input);
             tracker.process(input);
             if (!tracker.filterContoursOutput().isEmpty()) {
                 Rect boundingRect = Imgproc.boundingRect(tracker.filterContoursOutput().get(0));
-                double offset = (CAMERA_FOV / videoCapture.get(3)) * ((boundingRect.x + (boundingRect.width / 2.0)) - (videoCapture.get(3) / 2.0));
+                Point contourCenter = new Point(boundingRect.x + (boundingRect.width / 2.0), boundingRect.y + (boundingRect.height / 2.0));
+                Imgproc.drawMarker(input, contourCenter, new Scalar(0, 0, 255));
+                Imgproc.drawContours(input,tracker.filterContoursOutput(),-1,new Scalar(0, 0, 255),4);
+                double offset = (CAMERA_FOV/videoCapture.get(3)) * ((boundingRect.x + (boundingRect.width / 2.0)) - (videoCapture.get(3) / 2.0));
                 System.out.println("You are " + offset + " degrees from the target.");
             }
-            //httpStreamService.pushImage(input);
+            httpStreamService.pushImage(input);
         }
     }
 
