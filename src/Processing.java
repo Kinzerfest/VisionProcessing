3f76793299bd11e400ab184850eb2469c86d8703
Kevin Tran <26168327+Kinzerfest@users.noreply.github.com>
2018-11-04 16:39:45 -0600

    cleanup

--- src/Processing.java
+++ src/Processing.java
@@ -1,5 +1,4 @@
-import java.io.IOException;
-import org.opencv.core.*;
+import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.Rect;
 import org.opencv.imgproc.Imgproc;
@@ -11,33 +10,24 @@ public class Processing {
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
     }
 
-    static final double DISTANCE_CONSTANT = 1;
     static final int CAMERA_FOV = 100;
 
     static Pipeline tracker;
     public static VideoCapture videoCapture;
-    private static HttpStreamServer httpStreamService;
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         videoCapture = new VideoCapture();
         tracker = new Pipeline();
         videoCapture.open(1);
         Mat input = new Mat();
-        videoCapture.read(input);
-        httpStreamService = new HttpStreamServer(input);
-        httpStreamService.startStreamingServer();
         while (videoCapture.isOpened()) {
             videoCapture.read(input);
             tracker.process(input);
             if (!tracker.filterContoursOutput().isEmpty()) {
                 Rect boundingRect = Imgproc.boundingRect(tracker.filterContoursOutput().get(0));
-                Point contourCenter = new Point(boundingRect.x + (boundingRect.width / 2.0), boundingRect.y + (boundingRect.height / 2.0));
-                Imgproc.drawMarker(input, contourCenter, new Scalar(0, 0, 255));
-                Imgproc.drawContours(input,tracker.filterContoursOutput(),-1,new Scalar(0, 0, 255),4);
-                double offset = (CAMERA_FOV/videoCapture.get(3)) * ((boundingRect.x + (boundingRect.width / 2.0)) - (videoCapture.get(3) / 2.0));
+                double offset = (CAMERA_FOV / videoCapture.get(3)) * ((boundingRect.x + (boundingRect.width / 2.0)) - (videoCapture.get(3) / 2.0));
                 System.out.println("You are " + offset + " degrees from the target.");
             }
-            httpStreamService.pushImage(input);
         }
     }
 
