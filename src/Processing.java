0d600d976e214b505e700397d2e100470433424f
Kevin Tran <26168327+Kinzerfest@users.noreply.github.com>
2018-11-05 07:59:19 -0600

    Update src/Processing.java

--- src/Processing.java
+++ src/Processing.java
@@ -31,11 +31,13 @@ public class Processing {
                 Rect boundingRect = Imgproc.boundingRect(tracker.filterContoursOutput().get(0));
                 Point contourCenter = new Point(boundingRect.x + (boundingRect.width / 2.0), boundingRect.y + (boundingRect.height / 2.0));
                 Imgproc.drawMarker(input, contourCenter, new Scalar(0, 0, 255));
-                Imgproc.drawContours(input, tracker.filterContoursOutput(), -1, new Scalar(0, 0, 255), 4);
+                //Imgproc.rectangle(input,boundingRect.br(), boundingRect.tl(), new Scalar(0,0,255), 4);
+                //Imgproc.drawContours(input, tracker.filterContoursOutput(), -1, new Scalar(0, 0, 255), 4);
                 double offset = (CAMERA_FOV / videoCapture.get(3)) * ((boundingRect.x + (boundingRect.width / 2.0)) - (videoCapture.get(3) / 2.0));
-                System.out.println("You are " + offset + " degrees from the target.");
-                System.out.println("Area of contour: " + Imgproc.contourArea(tracker.filterContoursOutput().get(0)));
-                System.out.println("Area of bounding box: " + boundingRect.area());
+                Imgproc.putText(input, Integer.toString((int)offset) + " deg", contourCenter, 0, 0.65, new Scalar(0,0,255), 2);
+                //System.out.println("You are " + offset + " degrees from the target.");
+                //System.out.println("Area of contour: " + Imgproc.contourArea(tracker.filterContoursOutput().get(0)));
+                //System.out.println("Area of bounding box: " + boundingRect.area());
             }
             if (PUSH_TO_HTTP) httpStreamService.pushImage(input);
         }
